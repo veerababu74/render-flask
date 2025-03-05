e56647c8d5274fb679007a100723c1bdfd4b7a34
veerababu74 <veera99856@gmail.com>
2025-03-05 22:45:12 +0530

    new

--- Properties/urls.py
+++ Properties/urls.py
@@ -4,7 +4,7 @@ DEV_ENVIRONMENT = "LOCAL"
 
 FRONTEND_BASE_URL_FOR_CORS = "http://localhost:3000"
 FRONTEND_BASE_URL_LOCAL = "http://localhost:3000"
-FRONTEND_BASE_URL_PRODUCTION = "https://your-frontend-url.com"
+FRONTEND_BASE_URL_PRODUCTION = "https://reactfronend.onrender.com/"
 
 FRONTEND_BASE_URL = (
     FRONTEND_BASE_URL_PRODUCTION
@@ -12,13 +12,13 @@ FRONTEND_BASE_URL = (
     else FRONTEND_BASE_URL_LOCAL
 )
 
-API_BASE_URL = "http://localhost:5000"
+# API_BASE_URL = "http://localhost:5000"
 
-# Authentication URLs
-AUTH_URLS = {
-    "LOGIN_REDIRECT": f"{FRONTEND_BASE_URL}/login",
-    "EMAIL_VERIFICATION_SUCCESS": f"{FRONTEND_BASE_URL}/verify-success",
-    "PASSWORD_RESET": f"{FRONTEND_BASE_URL}/reset-password",
-    "GITHUB_CALLBACK": f"{FRONTEND_BASE_URL}/auth/github/callback",
-    "HELLO_WORLD": f"{FRONTEND_BASE_URL}/hello-world",
-}
+# # Authentication URLs
+# AUTH_URLS = {
+#     "LOGIN_REDIRECT": f"{FRONTEND_BASE_URL}/login",
+#     "EMAIL_VERIFICATION_SUCCESS": f"{FRONTEND_BASE_URL}/verify-success",
+#     "PASSWORD_RESET": f"{FRONTEND_BASE_URL}/reset-password",
+#     "GITHUB_CALLBACK": f"{FRONTEND_BASE_URL}/auth/github/callback",
+#     "HELLO_WORLD": f"{FRONTEND_BASE_URL}/hello-world",
+# }
